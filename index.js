a4d0d2958b2906d25c1eeaeca53775463652642a
DaviiJones <davonbridgett@pursuit.org>
2023-07-02 03:50:11 -0400

    needs more work

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("JavaScript is connected!");
+
+let buttons = document.getElementsByTagName("button");
+
+const cart = [];
+
+for(var i=0; i<buttons.length; i++) {
+  let button = buttons[i];
+  console.log(button);
+  button.addEventListener('click', function(event){
+    console.clear();
+    console.log(event.target);
+    console.log(event.target.dataset.productSku);
+    cart.push( event.target.dataset.productSku );
+    console.log(cart)    
+  });
+}
