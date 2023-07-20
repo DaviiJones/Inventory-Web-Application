588f5037d3427e2fef95adc1e9c530a4de047c3c
DaviiJones <davonbridgett@pursuit.org>
2023-07-19 22:38:53 -0400

    added working buttons, remove button, grid, hover features.

--- index.js
+++ index.js
@@ -1,17 +1,59 @@
-console.log("JavaScript is connected!");
+document.addEventListener("DOMContentLoaded", function () {
+  const buttons = document.querySelectorAll("button[data-product-sku]");
 
-let buttons = document.getElementsByTagName("button");
 
-const cart = [];
+  const cartItems = [];
 
-for(var i=0; i<buttons.length; i++) {
-  let button = buttons[i];
-  console.log(button);
-  button.addEventListener('click', function(event){
-    console.clear();
-    console.log(event.target);
-    console.log(event.target.dataset.productSku);
-    cart.push( event.target.dataset.productSku );
-    console.log(cart)    
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const productSku = button.getAttribute("data-product-sku");
+      const productName = button.parentElement.querySelector(".name").innerText;
+      const productPrice = parseFloat(button.parentElement.querySelector("#price").innerText);
+      const newItem = { sku: productSku, name: productName, price: productPrice };
+
+
+      const itemIndex = cartItems.findIndex((item) => item.sku === productSku);
+
+      if (itemIndex === -1) {
+        cartItems.push(newItem);
+      } else {
+
+        cartItems[itemIndex] = newItem;
+      }
+
+
+      displayCartItems();
+    });
   });
-}
+
+  function displayCartItems() {
+    const resultElement = document.querySelector(".result");
+    resultElement.innerHTML = "";
+
+    if (cartItems.length === 0) {
+      resultElement.innerHTML = "<p>Your cart is empty.</p>";
+      return;
+    }
+
+    const cartList = document.createElement("ul");
+    cartItems.forEach((item) => {
+      const cartItem = document.createElement("li");
+      cartItem.innerHTML = `
+        <strong>${item.name}</strong> - $${item.price.toFixed(2)}
+        <button class="remove-button">Remove</button>`;
+      cartList.appendChild(cartItem);
+    });
+
+    resultElement.appendChild(cartList);
+
+    const removeButtons = document.querySelectorAll(".remove-button");
+    removeButtons.forEach((removeButton) => {
+      removeButton.addEventListener("click", function () {
+        const itemIndex = this.getAttribute("data-item-index");
+        cartItems.splice(itemIndex, 1);
+        displayCartItems();
+      });
+    });
+  }
+});
